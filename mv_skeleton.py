5052d77d2311b8f8ff0b85ff2d0777eb6e75a4e0
dle100 <david.le22@hotmail.com>
2022-02-15 12:07:51 -0800

    attempting to create a loop to review multiple products

--- mv_skeleton.py
+++ mv_skeleton.py
@@ -1,7 +1,13 @@
+def get_product():
+    repeat = True
+    while repeat == True:
+        product = input(f"Which product would you like to review?\n")
+        mv_skeleton_convo(product)
+        repeat = review_Another()
+
 def mv_skeleton_convo(product):
     # Initial Prompt
-    question = input(f"Hello! Would you like to leave a review for the {product} you recently purchased?\n")
-    
+    question = input(f"Would you like to review {product}?\n")
     while True:
         # If user input is 'yes'
         # Go through the review process then break
@@ -53,6 +59,14 @@ def mv_skeleton_convo(product):
             break
         else:
             question = input("Didn't quite get that. Can you please indicate yes or no?\n")
-    
- 
-mv_skeleton_convo("Laptop")
+
+def review_Another():
+    reviewNextProduct = input(f"Would you like to review another product?")
+    while True:
+        if reviewNextProduct.lower() == 'yes':
+            return True
+        elif reviewNextProduct.lower() == 'no':
+            return False
+        else: 
+            reviewNextProduct = input(f"Didn't quite get that. Can you please indicate yes or no?\n")   
+
