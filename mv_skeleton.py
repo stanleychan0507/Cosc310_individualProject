8e0c34865fd71dad68cff9b9cea32035cb8cd3ee
rvanoerle <rvanoerle5@gmail.com>
2022-02-17 15:39:47 -0800

    Update mv_skeleton.py
    adjusted methods so you can review if satisfied or unsatisfied. Changed flow to return to the opening conversation when wanting to discuss another product, fixed a few minor bugs.

--- mv_skeleton.py
+++ mv_skeleton.py
@@ -1,9 +1,12 @@
+from urllib import response
+
+
 def opening_conversation():
     repeat = True
     while repeat == True:
-        product = input(f"Which product would you like to review?\n")
+        product = input(f"Which product would you like to discuss?\n")
         initial_conversation(product)
-        repeat = review_Another(product)
+        repeat = discuss_Another(product)
 def initial_conversation(product):
     # Initial Prompt
     question = input(f"Are you satisfied with {product}?\n")
@@ -12,67 +15,93 @@ def initial_conversation(product):
         # Go through the review process then break
         
         if question.lower() == "yes":
-            positive_review(product)
+            positive_feedback(product)
             break
         
         # If user input is 'no'
         # Ask if they are happy with their purchase
         elif question.lower() == "no":
-            negative_review(question)            
+            negative_feedback(question)            
             break
         else:
             question = input("Didn't quite get that. Can you please indicate yes or no?\n")
 
-def review_Another(product):
-    deep_review(product)
-    reviewNextProduct = input(f"Would you like to review another product?")
+def discuss_Another(product):
+    reviewNextProduct = input(f"Would you discuss another product?")
     while True:
         if reviewNextProduct.lower() == 'yes':
-            return True
+            opening_conversation()
+            break
         elif reviewNextProduct.lower() == 'no':
             print ("Thank you for your time")
-            return False
+            break
         else: 
             reviewNextProduct = input(f"Didn't quite get that. Can you please indicate yes or no?\n")   
-
-def positive_review(product):
-    rating = input(f"Great! How many stars would you rate the {product}? (0 - 10)\n")
-    recommend = input(f"Would you recommend {product} item to a friend?\n")
-    purchaseDate = input(f"When did you purchase the {product}?\n")
+        break
     
-    print(f"""Your review has been saved!
-    Rating: {rating}
-    Would you recommend: {recommend}
-    Date of Pruchase: {purchaseDate}
-    """)
-def negative_review(review):
+def positive_feedback(product):
+    ## If customer is satisfied with product
+    while True:
+        review = input(f"Great! Would you like to review the {product}?\n")
+        while True:
+            if review.lower() == "no":
+                break
+            elif review.lower() == "yes":
+                write_review(product)
+                break
+            else:
+                review = input("Didn't quite get that. Can you please indicate yes or no?\n")
+        break
+    
+def negative_feedback(product):
+    ## If customer is unsatisfied with product
     while True:
         # If they are not happy with their purchase
         # Ask if they want a refund
-        if review.lower() == "no":
-            refund = input("That's too bad. Would you like a refund?\n")
-            while True:
-                
+        refund = input("That's too bad. Would you like a refund?\n")
+        while True:
                 # If they want a refund, email will be sent. Break
                 if refund.lower() == "yes":
                     print("Okay, we'll send you an email shortly to begin the process")
                     break
-                
+
                 # If they want don't want a refund, break
                 elif refund.lower() == "no":
-                    print("Okay, have a good day!")
                     break
                 else: 
                     refund = input("Didn't quite get that. Can you please indicate yes or no?\n")
-            break
-        else:
-            review = input("Didn't quite get that. Can you please indicate yes or no?\n")
+               
+        # Ask if they want to review the product
+        review = input("Would you like to review the product?\n")
+        while True:
+            if review.lower() == "no":
+                break
+            elif review.lower() == "yes":
+                write_review(product)
+                break
+            else:
+                review = input("Didn't quite get that. Can you please indicate yes or no?\n")
+        break
+
+def write_review(product):
+    name = input(f"What is your name? \n")
+    rating = input(f"How many stars would you rate the {product}? (0 - 10)\n")
+    recommend = input(f"Would you recommend {product} item to a friend?\n")
+    purchaseDate = input(f"When did you purchase the {product}?\n")
+    
+    print(f"""Your review has been saved!
+    Name: {name}
+    Rating: {rating}
+    Would you recommend: {recommend}
+    Date of Pruchase: {purchaseDate}
+    """)
+    deep_review(product)
 
 def deep_review(product):
-    deepReview = input(f"Would you like to provide a comment on {product}?")
+    deepReview = input(f"Would you like to provide a comment on {product}?\n")
     while True:
         if deepReview.lower() == 'yes':
-            detailedReview = input(f"Please provide your comment on {product}")
+            detailedReview = input(f"Please provide your comment on {product}\n")
             print(f"""Your comment has been saved!\n
             Comment: {detailedReview}""")
             break
