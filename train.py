94395c4eed3ba399c9bd05b566feeb0aebd4a4d1
mvallido <vallido.michael@gmail.com>
2022-03-11 00:46:24 -0800

    Test bot with PyTorch and NLTK has been implemented

--- train.py
+++ train.py
@@ -90,3 +90,17 @@ for epoch in range(num_epochs):
         print(f'epoch {epoch + 1}/{num_epochs}, loss={loss.item():.4f}')
         
 print(f'final loss,  loss ={loss.item():.4f}')
+
+# Saving data
+data = {
+    "model_state": model.state_dict(),
+    "input_size": input_size,
+    "output_size": output_size,
+    "hidden_size": hidden_size,
+    "all_words": all_words,
+    "tags": tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+print(f'Training complete. File saved to {FILE}')
