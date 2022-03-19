e039dbba81edc82404b2d610c768b6aecaec85b2
unknown <stanleychan0507@yahoo.com.hk>
2022-03-18 23:29:18 -0700

    make changes for unit test

--- unitTest_Cosc310Project.py
+++ unitTest_Cosc310Project.py
@@ -1,30 +1,34 @@
+from pickle import APPEND
 import unittest
+import assignment_part2_david_Le as ChatBot
 
 class TestBot(unittest.TestCase):
-
+    @Test
     def setUp(self):
         # Load test data
-        self.app = App(database='assignment_part2_david_Le.py')
+        self.app = APPEND(chatbot='assignment_part2_david_Le.py')
 
     def test_writeReview(self):
-        # test that the data type in write reviews were valid
-        # e.g. 
-        # type(name)=str, 
-        # True if rating == 'y' else True if rating == 'n' else False, 
-        # True if recommending == 'y' else True if recommending == 'n' else False, , 
-        # type(purchaseDate)=str;
-        userInput = type(self.app.name())
-        self.assertEqual(userInput, 'str')
+        # Test case 1 : 
+        userInput = self.app.name()
+        self.assertEquals("inputString", userInput.notNull().asString())
 
-    # def test_output(self):
-    # do some test to see if the conversation flows
-    # e.g. in reviewMessage, is positiveService revoke if review > 7? .....etc.
+    def test_output(self):
+        # Test case 2 :
+        self.reviewMessage("pedicure")
+        self.assertEquals(1, positiveService("pedicure"))
 
-    # def test_negativeReview(self):
-    # do some tes to see if the output is correct.
-    # e.g. if answer is "yes", would it output deepservice(serviceName)?
-    # if answer is "no", would it print "Thanks for your feedback"
-    # if answer is either yes or no, would it print "Didn't quite get that....."
+    def test_negativeReview(self):
+        # Test case 3 :
+        self.rememberReview()
+        name = "Bob"
+        recommend = "y"
+        goodPoint = "goodPoint statement here"
+        feedback = "feedback statement here"
+        self.assertEquals(1, print("""Your review has been saved!
+            would you recommend {name}: {recommend}
+            what did we do well: {goodPoint}
+            how can we do better: {feedback}"""))
 
 if __name__ == '__main__':
     unnitest.main()
